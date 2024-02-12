6bea346ec582802f1e8d5a24934fd9879205dafb
ayinbota <ayinbota@users.noreply.github.com>
2024-02-12 23:59:00 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,19 +3,33 @@ const btn = document.querySelector(".btn");
 const clearBtn = document.querySelector(".clear-btn");
 const olist = document.querySelector(".ol-list");
 
+function updateList() {
+  const listItems = document.createElement("li");
+  listItems.textContent = inputEl.value;
+  olist.appendChild(listItems);
+
+  //alternative way of adding js to  html
+  //   olist.innerHTML += `<li>${inputEl.value}</li>`;
+
+  inputEl.value = "";
+}
+
+function pressEnterToSubmit() {
+  document.addEventListener("keydown", function (enter) {
+    if (enter.key === "Enter" && inputEl.value) {
+      updateList();
+    } else {
+    }
+  });
+}
+
 btn.addEventListener("click", function () {
   console.log(inputEl.value);
   if (!inputEl.value) {
     return;
   } else {
-    const listItems = document.createElement("li");
-    listItems.textContent = inputEl.value;
-    olist.appendChild(listItems);
-
-    //alternative way of adding js to  html
-    //   olist.innerHTML += `<li>${inputEl.value}</li>`;
-
-    inputEl.value = "";
+    updateList();
+    pressEnterToSubmit();
   }
 });
 
